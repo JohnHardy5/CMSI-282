cdc47b22256215edd055c390276d30ed414509ae
track394 <track394@yahoo.com>
2017-02-15 17:15:09 -0800

    Finished MathMethods and tests.

--- src/MathMethods/MathMethods.java
+++ src/MathMethods/MathMethods.java
@@ -34,7 +34,8 @@ public class MathMethods {
 	}
 	
 	static long lcm(long m, long n) {
-		return 0L;
+		if (m == 0) {return 0;}
+		return (m * n) / gcd(m, n);
 	}
 	
 	static double poly(double x, double[] coeff) {
@@ -46,15 +47,45 @@ public class MathMethods {
 	}
 	
 	static double power(double x, int n) {
-		return 0.0;
+		if (n == 0) {return 1.0;}
+		if (n == 1) {return x;}
+		if (n % 2 == 1) {
+			return x * power(x, n - 1);
+		} else {
+			double temp = power(x, n / 2);
+			return temp * temp;
+		}
 	}
 	
 	static double root(int n, double x, double epsilon) {
-		return 0.0;
+		if (n <= 0 || (x < 0 && n % 2 == 0) ) {throw new IllegalArgumentException("BAD DATA");}
+		if (x == 1.0) {return 1.0;}
+		double lowerBnd = 0;
+		double upperBnd = (x > 0 && x < 1) ? 1.0 : x;
+		double midPnt = setMidPoint(lowerBnd, upperBnd);
+		double estimate = power(midPnt, n);
+		while (isOutsideEpsilon(estimate, x, epsilon)) {
+			if (estimate > x) {
+				upperBnd = midPnt;
+			} else {
+				lowerBnd = midPnt;
+			}
+			midPnt = setMidPoint(lowerBnd, upperBnd);
+			estimate = power(midPnt, n);
+		}
+		return midPnt;
+	}
+	
+	private static double setMidPoint(double lowerBnd, double upperBnd) {
+		return  (lowerBnd + upperBnd) / 2.0;
+	}
+	
+	private static boolean isOutsideEpsilon(double estimate, double x, double epsilon) {
+		return Math.abs(estimate - x) > epsilon;
 	}
 	
 	static double sqrt(double x, double epsilon) {
-		return 0.0;
+		return root(2, x, epsilon);
 	}
 	
 	public static void main(String[] args) {
@@ -96,7 +127,7 @@ public class MathMethods {
     		));
     		break;
     	default:
-    		throw new IllegalArgumentException("Given operation was not specified correctly.");
+    		throw new IllegalArgumentException("BAD DATA");
     	}
     }
 	
--- src/MathMethods/MathMethodsTests.java
+++ src/MathMethods/MathMethodsTests.java
@@ -43,7 +43,7 @@ public class MathMethodsTests {
 	public void testLcm() {
 		assertEquals(0L, MathMethods.lcm(0, 0));
 		assertEquals(1L, MathMethods.lcm(1, 1));
-		assertEquals(8L, MathMethods.lcm(2, 4));
+		assertEquals(4L, MathMethods.lcm(2, 4));
 		assertEquals(90L, MathMethods.lcm(30, 45));
 		assertEquals(240L, MathMethods.lcm(12, 80));
 	}
@@ -59,11 +59,11 @@ public class MathMethodsTests {
 	@Test
 	public void testPower() {
 		assertEquals(1.0, MathMethods.power(0.0, 0), 0.001);
-		assertEquals(1.0, MathMethods.power(1.0, 1000000000), 0.001);
+		assertEquals(1.0, MathMethods.power(1.0, 1000000000), 0.01);
 		assertEquals(4.0, MathMethods.power(2.0, 2), 0.001);
-		assertEquals(9.86, MathMethods.power(Math.PI, 2), 0.001);
-		assertEquals(1096.63, MathMethods.power(Math.E, 7), 0.001);
-		assertEquals(1.2528328e25, MathMethods.power(123.45, 12), 0.001);
+		assertEquals(9.86, MathMethods.power(Math.PI, 2), 0.01);
+		assertEquals(1096.63, MathMethods.power(Math.E, 7), 0.01);
+		assertEquals(1.2528328e25, MathMethods.power(123.45, 12), 1e18);
 	}
 	
 	@Test
@@ -71,15 +71,17 @@ public class MathMethodsTests {
 		assertEquals(0.2, MathMethods.root(1, 0.2, 0.001), 0.001);
 		assertEquals(3.0, MathMethods.root(3, 27.0, 0.001), 0.001);
 		assertEquals(2.0, MathMethods.root(24, 16777216.0, 0.001), 0.001);
-		assertEquals(2.14, MathMethods.root(8, 456.02, 0.001), 0.001);
+		assertEquals(0.9594820623236890, MathMethods.root(8, Math.E - 2.0, 1e-15), 1e-15);
+		assertEquals(2.14967589341, MathMethods.root(8, 456.02, 1e-10), 1e-10);
 	}
 	
 	@Test
 	public void testSqrt() {
-		assertEquals(0.0, MathMethods.sqrt(0.0, 0.001), 0.001);
-		assertEquals(2.0, MathMethods.sqrt(4.0, 0.001), 0.001);
-		assertEquals(10.0, MathMethods.sqrt(100.0, 0.001), 0.001);
-		assertEquals(1.64, MathMethods.sqrt(Math.E, 0.001), 0.001);
+		assertEquals(0.0, MathMethods.sqrt(0.0, 0.01), 0.01);
+		assertEquals(2.0, MathMethods.sqrt(4.0, 0.001), 0.01);
+		assertEquals(10.0, MathMethods.sqrt(100.0, 0.001), 0.01);
+		assertEquals(0.37628, MathMethods.sqrt(Math.PI - 3.0, 1e-5), 1e-5);
+		assertEquals(1.7724538509055160, MathMethods.sqrt(Math.PI, 1e-15), 1e-15);
 	}
 	
 }
